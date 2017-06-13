1429a2a7bcf887e4e51eee4d5787a09135c34932
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-06-13 03:03:01 -0500

    tic tac toy game

---
+++ Michi-TicTacgame/app/src/main/java/com/kotlingame/enzoftware/michi_tictacgame/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.kotlingame.enzoftware.michi_tictacgame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buClick(view: View){
+        val buSelected:Button = view as Button
+        var cellID:Int = 0
+        when(buSelected.id){
+            R.id.bu1 -> cellID = 1
+            R.id.bu2 -> cellID = 2
+            R.id.bu3 -> cellID = 3
+            R.id.bu4 -> cellID = 4
+            R.id.bu5 -> cellID = 5
+            R.id.bu6 -> cellID = 6
+            R.id.bu7 -> cellID = 7
+            R.id.bu8 -> cellID = 8
+            R.id.bu9 -> cellID = 9
+        }
+
+        Toast.makeText(this,"ID : "+cellID,Toast.LENGTH_SHORT).show()
+    }
+
+}
